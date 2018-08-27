5e5a4ee34a62f473f463d5aaee18b1ac9c87232f
Pietr00 <nalbertpietro@gmail.com>
2018-08-27 15:54:00 -0300

    Buttons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 
-import "./App.css";
-import Header from "./components/Header";
-import NewUser from "./components/NewUser";
+import './App.css';
+import Header from './components/Header';
+import NewUser from './components/NewUser';
+import Toast from './components/Toast';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
-        <NewUser />
+        <NewUser error={msg => this.refs.toast.error(msg)} />
+        <Toast ref="toast" />
       </div>
     );
   }
