6f0f98a895b35020a6a86c035351243593fb123d
htmlucas <lucas.emanoell@outlook.com>
2018-11-12 14:54:13 -0300

    PRONTO <3

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./css/index.css";
-import "./css/pure.css";
-import "./img/icon-72x72.png";
-import "./img/icon-96x96.png";
-import "./img/icon-128x128.png";
-import "./img/icon-144x144.png";
-import "./img/icon-152x152.png";
-import "./img/icon-192x192.png";
-import "./img/icon-384x384.png";
-import "./img/icon-512x512.png";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import './css/pure.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
